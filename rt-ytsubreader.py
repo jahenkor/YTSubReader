60ddae7940951f29b54d0fc9ebcd88e64038a90e
altrustic <altrustic@AltruistShade.AltruestShade>
2017-11-26 13:04:03 -0500

    Fixed syntax error

--- rt-ytsubreader.py
+++ rt-ytsubreader.py
@@ -40,8 +40,8 @@ found at:
       """ % os.path.abspath(os.path.join(os.path.dirname(__file__),
                                               CLIENT_SECRETS_FILE))
 
-       # This OAuth 2.0 access scope allows for full read/write access to the
-       # authenticated user's account.
-       YOUTUBE_SCOPE = "https://www.googleapis.com/auth/youtube"
-       YOUTUBE_API_SERVICE_NAME = "youtube"
-       YOUTUBE_API_VERSION = "v3"
+# This OAuth 2.0 access scope allows for full read/write access to the
+# authenticated user's account.
+YOUTUBE_SCOPE = "https://www.googleapis.com/auth/youtube"
+YOUTUBE_API_SERVICE_NAME = "youtube"
+YOUTUBE_API_VERSION = "v3"
